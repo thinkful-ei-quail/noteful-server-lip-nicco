82de9cf9d39822bf0a8ad19a4db188af9a3ea85e
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2020-08-25 13:04:55 -0700

    moving errors in errorHandler

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
+  "ssl": {
+    rejectUnauthorized: true
+  },
   "connectionString": (process.env.NODE_ENV === 'test')
     ? process.env.TEST_DATABASE_URL
     : process.env.DATABASE_URL,
